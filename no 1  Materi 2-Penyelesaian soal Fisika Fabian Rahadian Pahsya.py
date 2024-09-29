5bd8000eac956a5e7459e68c32a6eced9ae7852d
Fabian Rahadian <fpashya@gmail.com>
2024-09-29 22:09:26 +0700

    Add files via upload

---
+++ no 1  Materi 2-Penyelesaian soal Fisika Fabian Rahadian Pahsya.py
@@ -0,0 +1,14 @@
+# Diketahui
+n = 1.50  # indeks bias medium
+R1 = 22  # jari-jari kelengkungan permukaan lensa R1 (dalam cm)
+R2 = 17.5  # jari-jari kelengkungan permukaan lensa R2 (dalam cm)
+
+# Menggunakan persamaan pembuat lensa
+# 1/f = (n - 1) * (1/R1 + 1/R2)
+
+f_inverse = (n - 1) * (1/R1 + 1/R2)  # menghitung 1/f
+f = 1 / f_inverse  # menghitung jarak fokus f
+
+f  # hasil jarak fokus lensa (f) dalam cm
+
+print(" Jarak Fokus Lensa : ",f, "cm")
